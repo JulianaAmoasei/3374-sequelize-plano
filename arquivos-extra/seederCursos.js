b2c899f854a0cb839c72ecf65c4071361b4a9a9f
JulianaAmoasei <late.parrot@gmail.com>
2023-08-23 17:52:35 -0300

    arquivos iniciais

---
+++ arquivos-extra/seederCursos.js
@@ -0,0 +1,38 @@
+[
+  {
+    titulo: "API com Express",
+    descricao: "Curso de API com Express e MongoDB",
+    dataInicio: "2023-01-01",
+    categoriaId: 1,
+    docenteId: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    titulo: "SpringBoot",
+    descricao: "Curso de Java com Spring Framework",
+    dataInicio: "2023-01-01",
+    categoriaId: 2,
+    docenteId: 5,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    titulo: "Python Web com Django",
+    descricao: "Curso de aplicações web com Django",
+    dataInicio: "2023-01-01",
+    categoriaId: 3,
+    docenteId: 6,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    titulo: "Orientação a Objetos com C#",
+    descricao: "Curso de C#: coleções, arquivos e libs",
+    dataInicio: "2023-01-01",
+    categoriaId: 4,
+    docenteId: 6,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
---
+++ arquivos-extra/seederMatriculas.js
@@ -0,0 +1,30 @@
+[
+  {
+    estudanteId: 1,
+    cursoId: 1,
+    status: "matriculado",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    estudanteId: 2,
+    cursoId: 2,
+    status: "matriculado",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    estudanteId: 3,
+    cursoId: 3,
+    status: "matriculado",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    estudanteId: 4,
+    cursoId: 4,
+    status: "matriculado",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
