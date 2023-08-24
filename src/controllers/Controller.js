57d39c132176ddf32423456b6ae2ffb343ba2b4c
JulianaAmoasei <late.parrot@gmail.com>
2023-08-24 15:55:30 -0300

    video 3.5

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -12,6 +12,51 @@ class Controller {
       return res.status(500).json(erro.message);
     }
   }
+
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+      return res.status(200).json(umRegistro);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  async criaNovo(req, res) {
+    const dadosParaCriacao = req.body;
+    try {
+      const novoCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
+      return res.status(200).json(novoCriado);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, id);
+      if (!foiAtualizado) {
+        return res.status(400).json({ message: 'registro não foi atualizado' });
+      }
+      return res.json({ message: 'Atualizado com sucesso' });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.excluiRegistro(id);
+      return res.status(200).json({ mensagem: `id ${id} deletado` });
+
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -9,30 +9,30 @@ class Services {
     return dataSource[this.model].findAll({ where: { ...where } });
   }
 
-  // async pegaUmRegistroPorId(id) {
-  //   return dataSource[this.model].findByPk(id);
-  // }
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
 
-  // async pegaUmRegistro(where) {
-  //   return dataSource[this.model].findOne({ where: { ...where } });
-  // }
+  async pegaUmRegistro(where) {
+    return dataSource[this.model].findOne({ where: { ...where } });
+  }
 
-  // async criaRegistro(dadosDoRegistro) {
-  //   return dataSource[this.model].create(dadosDoRegistro);
-  // }
+  async criaRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
 
-  // async atualizaRegistro(dadosAtualizados, where){
-  //   const registrosAtualizados = dataSource[this.model]
-  //     .update(dadosAtualizados, { where: { ...where } });
-  //   if (registrosAtualizados[0] === 0) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+  async atualizaRegistro(dadosAtualizados, where){
+    const registrosAtualizados = dataSource[this.model]
+      .update(dadosAtualizados, { where: { ...where } });
+    if (registrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
 
-  // async excluiRegistro(id) {
-  //   return dataSource[this.model].destroy({ where: { id: id } });
-  // }
+  async excluiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
 }
 
 module.exports = Services;
